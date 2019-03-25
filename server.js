8f866d05674e9a80aede687b86839bbb9ff11953
sheylao <sheypr85@gmail.com>
2019-03-25 00:30:58 -0500

    Adding db

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 var mongoose = require("mongoose");
 
-// var db = require("./models");
+var db = require("./models");
 
 var PORT = 3000;
 
@@ -17,6 +17,8 @@ app.use(express.urlencoded({
 app.use(express.json());
 // Make public a static folder
 // app.use(express.static("public"));
+mongoose.connect("mongodb://localhost/NewsScraper", { useNewUrlParser: true });
+
 
 // Routes
 
@@ -50,14 +52,38 @@ app.get("/scrape", function (req, res) {
                 .find("div.col-2.assetThumb a figure.img span img").attr("data-original")
 
 
-            // Create a new Article using the `result` object built from scraping
             console.log(result)
+
+            // Create a new Article using the `result` object built from scraping
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    // View the added result in the console
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    // If an error occurred, log it
+                    console.log(err);
+                });
         });
 
         // Send a message to the client
         res.send("Scrape Complete");
     });
 });
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
 
 
 app.listen(PORT, function () {
