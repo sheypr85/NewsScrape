c1fdd6ae4fce9cde518ab8676afd9eb1af6b29a4
sheylao <sheypr85@gmail.com>
2019-03-24 21:39:35 -0500

    Adding required dependencies to server.js

--- server.js
+++ server.js
@@ -3,5 +3,17 @@ var express = require("express");
 var mongojs = require("mongojs");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var mongoose = require("mongoose");
 
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
 var app = express();
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
