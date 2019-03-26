330dda73bbd66764505059b25a51a78dae9a8dcc
sheylao <sheypr85@gmail.com>
2019-03-26 01:41:26 -0500

    Fixing views

--- models/Article.js
+++ models/Article.js
@@ -19,6 +19,9 @@ var ArticleSchema = new Schema({
   picture: {
       type: String
   },
+  summary: {
+    type: String
+  },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 // Make public a static folder
-// app.use(express.static("public"));
+app.use(express.static("public"));
 mongoose.connect("mongodb://localhost/NewsScraper", { useNewUrlParser: true });
 
 
@@ -71,19 +71,19 @@ app.get("/scrape", function (req, res) {
     });
 });
 
-// Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-    // Grab every document in the Articles collection
-    db.Article.find({})
-      .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
-        res.json(dbArticle);
-      })
-      .catch(function(err) {
-        // If an error occurred, send it to the client
-        res.json(err);
-      });
-  });
+// // Route for getting all Articles from the db
+// app.get("/articles", function(req, res) {
+//     // Grab every document in the Articles collection
+//     db.Article.find({})
+//       .then(function(dbArticle) {
+//         // If we were able to successfully find Articles, send them back to the client
+//         res.json(dbArticle);
+//       })
+//       .catch(function(err) {
+//         // If an error occurred, send it to the client
+//         res.json(err);
+//       });
+//   });
 
 
 app.listen(PORT, function () {
